7d0795fe517ea913d1c6c40da273e6683539a03d
zackarno <zackarno@gmail.com>
2022-02-12 17:11:28 -0500

    add date to band names for each image in imageCollection. Necessary for identifying dates after `ee_extract`

---
+++ R/map_date_to_band_name_ic.R
@@ -0,0 +1,22 @@
+map_date_to_bandname_ic <- function(ic){
+  ic |>
+    ee$ImageCollection$map(
+      function(x){
+        # can't use getInfo() in sever-side function
+        bnames<- x$bandNames()
+        date <- ee$Date(x$get("system:time_start"))$format('YYYY_MM_dd')
+
+        # since bnames is technically a list rather than a simple string I need to map over it
+        # this should make it flexible fore when there are more bans I want to rename anyways
+        bnames_date <- bnames$map(
+          rgee::ee_utils_pyfunc(function(x){
+            ee$String(x)$cat(ee$String("_"))$cat(date)
+
+          })
+        )
+        x$select(bnames)$rename(bnames_date)
+      }
+
+    )
+
+}

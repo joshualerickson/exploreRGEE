a4eed3b03e1d201200d972776bb6311277551f23
zackarno <zackarno@gmail.com>
2022-02-16 17:42:45 -0500

    update year_filter

--- R/utils_filters.R
+++ R/utils_filters.R
@@ -65,75 +65,31 @@ months = ee$List$sequence(c.low, c.high)
 
 }
 #filter by year
-year_filter <- function(startDate, endDate, imageCol, stat){
+year_filter <-  function(startDate, endDate, imageCol, stat){
 
   startYear = lubridate::year(startDate)
   endYear = lubridate::year(endDate)
-
   years = ee$List$sequence(startYear, endYear)
 
-  if(stat == "median"){
+  ee_reducer <-  convert_reducer(stat)
 
-    byYear = ee$ImageCollection$fromImages(
-      years$map(rgee::ee_utils_pyfunc(function (y) {
-        indexString = ee$Number(y)$format('%03d')
-        return(imageCol$filter(ee$Filter$calendarRange(y, y, 'year'))$median()$set('system:index', indexString))}
-      )))
+  ee$ImageCollection$fromImages(
+    years$map(rgee::ee_utils_pyfunc(function (y) {
+      indexString = ee$Number(y)$format('%03d')
+      ic_temp_filtered <- imageCol$filter(ee$Filter$calendarRange(y, y, 'year'))
+      ee_reducer(ic_temp_filtered)$
+        set('system:index', indexString)$
+        set('year',y)$
+        set('month',1)$
+        set('date',ee$Date$fromYMD(y,1,1))$
+        # set('system:time_start',ee$Date$fromYMD(y,m,1))$
+        set('system:time_start',ee$Date$millis(ee$Date$fromYMD(y,1,1)))
+    }
 
-  } else if (stat == "mean") {
-
-    byYear = ee$ImageCollection$fromImages(
-      years$map(rgee::ee_utils_pyfunc(function (y) {
-        indexString = ee$Number(y)$format('%03d')
-        return(imageCol$filter(ee$Filter$calendarRange(y, y, 'year'))$mean()$set('system:index', indexString))}
-      )))
-
-  } else if (stat == "max") {
-
-    byYear = ee$ImageCollection$fromImages(
-      years$map(rgee::ee_utils_pyfunc(function (y) {
-        indexString = ee$Number(y)$format('%03d')
-        return(imageCol$filter(ee$Filter$calendarRange(y, y, 'year'))$max()$set('system:index', indexString))}
-      )))
-
-  } else if (stat == "min") {
-
-    byYear = ee$ImageCollection$fromImages(
-      years$map(rgee::ee_utils_pyfunc(function (y) {
-        indexString = ee$Number(y)$format('%03d')
-        return(imageCol$filter(ee$Filter$calendarRange(y, y, 'year'))$min()$set('system:index', indexString))}
-      )))
-
-  } else if (stat == "sum"){
-
-    byYear = ee$ImageCollection$fromImages(
-      years$map(rgee::ee_utils_pyfunc(function (y) {
-        indexString = ee$Number(y)$format('%03d')
-        return(imageCol$filter(ee$Filter$calendarRange(y, y, 'year'))$sum()$set('system:index', indexString))}
-      )))
-
-  } else if (stat == "stdDev"){
-
-    byYear = ee$ImageCollection$fromImages(
-      years$map(rgee::ee_utils_pyfunc(function (y) {
-        indexString = ee$Number(y)$format('%03d')
-        statImage = imageCol$filter(ee$Filter$calendarRange(y, y, 'year'))$reduce(ee$Reducer$stdDev())
-        return(statImage$set('system:index', indexString))}
-      )))
-
-
-  } else if (stat == 'first'){
-
-    byYear = ee$ImageCollection$fromImages(
-      years$map(rgee::ee_utils_pyfunc(function (y) {
-        indexString = ee$Number(y)$format('%03d')
-        return(imageCol$filter(ee$Filter$calendarRange(y, y, 'year'))$first()$set('system:index', indexString))}
-      )))
-
-  }
-
-
+    ))
+  )
 }
+
 
 # function that will reduce the month per year //
 
